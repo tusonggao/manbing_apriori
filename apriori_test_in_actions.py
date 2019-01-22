9582063df5f8042a5568305f89aecbefe900b9bd
tusonggao <tusonggao@163.com>
2019-01-22 19:25:11 +0800

    update files

--- apriori_test_in_actions.py
+++ apriori_test_in_actions.py
@@ -10,7 +10,7 @@ def createC1(dataSet):
         for item in transaction:
             if not [item] in C1:
                 C1.append([item])
-            C1.sort()
+    C1.sort()
     return map(frozenset, C1)
 
 def scanD(D, Ck, minSupport):
@@ -18,44 +18,19 @@ def scanD(D, Ck, minSupport):
     for tid in D:
         for can in Ck:
             if can.issubset(tid):
-                if can not in ssCnt: ssCnt[can]=1
-                else: ssCnt[can] += 1
+                if can not in ssCnt:
+                    ssCnt[can]=1
+                else:
+                    ssCnt[can] += 1
     numItems = float(len(D))
     retList = []
     supportData = {}
     for key in ssCnt:
         support = ssCnt[key]/numItems
         if support >= minSupport:
-            retList.insert(0,key)
+            retList.insert(0, key)
         supportData[key] = support
     return retList, supportData
-
-
-def aprioriGen(Lk, k): #creates Ck
-    retList = []
-    lenLk = len(Lk)
-    for i in range(lenLk):
-        for j in range(i+1, lenLk):
-            L1 = list(Lk[i])[:k-2]; L2 = list(Lk[j])[:k-2]
-            L1.sort(); L2.sort()
-            if L1==L2:
-                retList.append(Lk[i] | Lk[j])
-    return retList
-
-def apriori(dataSet, minSupport = 0.5):
-    C1 = createC1(dataSet)
-    D = map(set, dataSet)
-    L1, supportData = scanD(D, C1, minSupport)
-    L = [L1]
-
-    k = 2
-    while (len(L[k - 2]) > 0):
-        Ck = aprioriGen(L[k - 2], k)
-    Lk, supK = scanD(D, Ck, minSupport)
-    supportData.update(supK)
-    L.append(Lk)
-    k += 1
-    return L, supportData
 
 def aprioriGen(Lk, k): #creates Ck
     retList = []
@@ -69,7 +44,6 @@ def aprioriGen(Lk, k): #creates Ck
             if L1==L2:
                 retList.append(Lk[i] | Lk[j])
     return retList
-
 
 def apriori(dataSet, minSupport=0.5):
     C1 = createC1(dataSet)
@@ -98,6 +72,7 @@ def generateRules(L, supportData, minConf=0.7):
     return bigRuleList
 
 
+
 def calcConf(freqSet, H, supportData, brl, minConf=0.7):
     prunedH = []
     for conseq in H:
@@ -107,6 +82,8 @@ def calcConf(freqSet, H, supportData, brl, minConf=0.7):
             brl.append((freqSet-conseq, conseq, conf))
             prunedH.append(conseq)
     return prunedH
+
+
 
 def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
     m = len(H[0])
@@ -148,3 +125,8 @@ print(voteList[22])
 
 for key in transDict.keys():
     print(transDict[key])
+
+
+if __name__=='__name__':
+    print('helo world!')
+    prin('otput apriori fthemem')
