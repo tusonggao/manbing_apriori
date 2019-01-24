8ce06866a7f420e1517839c8f3a44f999ebb5c39
tusonggao <tusonggao@163.com>
2019-01-24 10:05:31 +0800

    update files

--- apriori_test_in_actions.py
+++ apriori_test_in_actions.py
@@ -1,8 +1,39 @@
-from votesmart import votesmart
 
+from numpy import *
+import pandas as pd
 
 def loadDataSet():
-    return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+    # return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+
+    # return [[1, 3, 4],
+    #         [2, 3, 5],
+    #         [1, 2, 3, 5],
+    #         [2, 5],
+    #         [3, 4, 7, 9],
+    #         [1, 4, 8],
+    #         [2, 3, 4],
+    #         [2, 5],
+    #         [2, 3],
+    #         [3, 7],
+    #         [1, 3, 4],
+    #         [3, 2, 7]]
+
+    # return [[1, 3, 4],
+    #         [3, 4, 5],
+    #         [1, 2, 3, 4],
+    #         [2, 4],
+    #         [3, 4, 1, 2],
+    #         [3, 4, 5],
+    #         [2, 3, 4]]
+
+    return [['1', '3', '4'],
+            ['3', '4', '5'],
+            ['1', '2', '3', '4'],
+            ['2', '4'],
+            ['3', '4', '1', '2'],
+            ['3', '4', '5'],
+            ['2', '3', '4']]
+
 
 def createC1(dataSet):
     C1 = []
@@ -10,8 +41,11 @@ def createC1(dataSet):
         for item in transaction:
             if not [item] in C1:
                 C1.append([item])
+
     C1.sort()
-    return map(frozenset, C1)
+    return list(map(frozenset, C1))  # use frozen set so we
+    # can use it as a key in a dict
+
 
 def scanD(D, Ck, minSupport):
     ssCnt = {}
@@ -19,50 +53,53 @@ def scanD(D, Ck, minSupport):
         for can in Ck:
             if can.issubset(tid):
                 if can not in ssCnt:
-                    ssCnt[can]=1
+                    ssCnt[can] = 1
                 else:
                     ssCnt[can] += 1
     numItems = float(len(D))
     retList = []
     supportData = {}
     for key in ssCnt:
-        support = ssCnt[key]/numItems
+        support = ssCnt[key] / numItems
         if support >= minSupport:
             retList.insert(0, key)
         supportData[key] = support
     return retList, supportData
 
-def aprioriGen(Lk, k): #creates Ck
+
+def aprioriGen(Lk, k):  # creates Ck
     retList = []
     lenLk = len(Lk)
-    for i in range(LenLk):
-        for j in range(i+1, lenLk):
-            L1 = list(Lk[i])[:k-2]
-            L2 = list(Lk[j])[:k-2]
-            L1.sort()
+    for i in range(lenLk):
+        for j in range(i + 1, lenLk):
+            L1 = list(Lk[i])[:k - 2];
+            L2 = list(Lk[j])[:k - 2]
+            L1.sort();
             L2.sort()
-            if L1==L2:
-                retList.append(Lk[i] | Lk[j])
+            if L1 == L2:  # if first k-2 elements are equal
+                retList.append(Lk[i] | Lk[j])  # set union
     return retList
+
 
 def apriori(dataSet, minSupport=0.5):
     C1 = createC1(dataSet)
     D = map(set, dataSet)
+    D = list(D) # added by tusonggao
     L1, supportData = scanD(D, C1, minSupport)
     L = [L1]
     k = 2
-    while(len(L[k-2])>0):
-        Ck = aprioriGen(L[k-2], k)
-        Lk, supK = scanD(D, Ck, minSupport)
+    while (len(L[k - 2]) > 0):
+        Ck = aprioriGen(L[k - 2], k)
+        Lk, supK = scanD(D, Ck, minSupport)  # scan DB to get Lk
         supportData.update(supK)
         L.append(Lk)
         k += 1
     return L, supportData
 
 
-def generateRules(L, supportData, minConf=0.7):
+def generateRules(L, supportData, minConf=0.7):  # supportData is a dict coming from scanD
     bigRuleList = []
-    for i in range(1, len(L)):
+    for i in range(1, len(L)):  # only get the sets with two or more items
         for freqSet in L[i]:
             H1 = [frozenset([item]) for item in freqSet]
             if (i > 1):
@@ -72,61 +109,55 @@ def generateRules(L, supportData, minConf=0.7):
     return bigRuleList
 
 
-
 def calcConf(freqSet, H, supportData, brl, minConf=0.7):
-    prunedH = []
+    prunedH = []  # create new list to return
     for conseq in H:
-        conf = supportData[freqSet]/supportData[freqSet-conseq]
+        conf = supportData[freqSet] / supportData[freqSet - conseq]  # calc confidence
         if conf >= minConf:
-            print(freqSet-conseq,'-->',conseq,'conf:',conf)
-            brl.append((freqSet-conseq, conseq, conf))
+            print(freqSet - conseq, '-->', conseq, 'conf:', conf)
+            brl.append((freqSet - conseq, conseq, conf))
             prunedH.append(conseq)
     return prunedH
 
 
-
 def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
     m = len(H[0])
-    if (len(freqSet) > (m + 1)):
-        Hmp1 = aprioriGen(H, m + 1)
-    Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
-    if (len(Hmp1) > 1):
-        rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
+    if (len(freqSet) > (m + 1)):  # try further merging
+        Hmp1 = aprioriGen(H, m + 1)  # create Hm+1 new candidates
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if (len(Hmp1) > 1):  # need at least two sets to merge
+            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
 
 
-#
+def pntRules(ruleList, itemMeaning):
+    for ruleTup in ruleList:
+        for item in ruleTup[0]:
+            print(itemMeaning[item])
+        print("-------->")
+        for item in ruleTup[1]:
+            print(itemMeaning[item])
+        print("confidence: %f" % ruleTup[2])
+        print()  # print a blank line
+
+######################################################################
+
+def get_orders_product():
+    return
+
+######################################################################
+
+
 # dataSet=loadDataSet()
 # print('dataSet is', dataSet)
-#
-# C1=createC1(dataSet)
-# print('C1 is', C1)
-#
-# D=map(set, dataSet)
-# print('D is', D)
-#
-# L1, suppData0=scanD(D, C1, 0.5)
-# print(L1)
-#
-# C1 = createC1(dataSet)
-#
-# L, suppData = apriori(dataSet)
 
-votesmart.apikey = '49024thereoncewasamanfromnantucket94040'
-bills = votesmart.votes.getBillsByStateRecent()
+# result = createC1(dataSet)
+# print(result)
 
-for bill in bills:
-    print(bill.title,bill.billId)
+L, suppData=apriori(dataSet, minSupport=0.33)
+rules=generateRules(L,suppData, minConf=0.20)
+print(L, suppData)
 
-bill = votesmart.votesmart.votes.getBill(11820)
-print(bill.actions)
-
-voteList = votesmart.votesmart.votes.getBillActionVotes(31670)
-print(voteList[22])
-
-for key in transDict.keys():
-    print(transDict[key])
+print('rules is', rules)
 
 
-if __name__=='__name__':
-    print('helo world!')
-    prin('otput apriori fthemem')
+
--- get_order_products.py
+++ get_order_products.py
@@ -2,8 +2,12 @@ import efficient_apriori
 import pandas as pd
 import numpy as np
 import time
+import json
 
+from efficient_apriori import apriori
 from collections import OrderedDict
+
+useless_common_titles = {'售后服务卡', '健客大药房旗舰店宣传单', '健客家庭药箱'}
 
 def get_suits_product_info():
     start_t = time.time()
@@ -24,6 +28,10 @@ def get_orders_product_info():
     orders_product_info = {}
     df_orders_product = pd.read_csv('./data/hive_sql_orders_products_data.csv')
     df_orders_product = df_orders_product[['orders_code', 'product_code']]
+
+    df_orders_product.sort_values(by=['orders_code'], ascending=False, inplace=True)
+    # df_orders_product = df_orders_product[:3000]
+
     df_new = df_orders_product.groupby('orders_code')['product_code'].apply(list)
 
     for index, row in df_new.items():  # 获取每行的index、row
@@ -34,19 +42,87 @@ def get_orders_product_info():
 
 
 def split_suit_product(orders_product_info, suits_product_info):
-    return orders_product_info
+    print('in split_suit_product')
+    orders_product_info_new = {}
+    for orders_code in orders_product_info:
+        orders_product_info_new[orders_code] = []
+        for product_code in orders_product_info[orders_code]:
+            if product_code in suits_product_info:
+                orders_product_info_new[orders_code] += suits_product_info[product_code]
+            else:
+                orders_product_info_new[orders_code].append(product_code)
+
+    return orders_product_info_new
 
 
-def map_product_code_2_common_titles(orders_product_info, )
+def map_product_code_2_common_titles(orders_product_info):
+    print('in map_product_code_2_common_titles')
+    df_product_name_map = pd.read_csv('./data/hive_sql_products_info_data.csv')
+    # df_product_name_map = df_product_name_map.sample(15)
+    df_product_name_map = df_product_name_map[['product_code', 'common_title']]
+    df_product_name_map = df_product_name_map.set_index('product_code')
+    df_product_name_map = df_product_name_map.to_dict()['common_title']
+    # print('df_product_name_map is ', df_product_name_map)
+
+    correct_cnt = 0
+    err_cnt = 0
+
+    orders_product_info_new = {}
+    for order_code in orders_product_info:
+        common_title_set = set()
+        for product_code in orders_product_info[order_code]:
+            try:
+                common_title = df_product_name_map[product_code]
+                # print('product_code is {0} common_title is {1}'.format(product_code, common_title))
+                if common_title not in useless_common_titles:
+                    common_title_set.add(common_title)
+                correct_cnt += 1
+            except KeyError:
+                err_cnt += 1
+                # print('Oops! product_code: {0} not in df_product_name_map'.format(product_code))
+        orders_product_info_new[order_code] = list(common_title_set)
+
+    print('err_cnt is ', err_cnt, 'correct_cnt is ', correct_cnt)
+    return orders_product_info_new
 
 
+# orders_product_info = get_orders_product_info()
+# print('orders_product_info is ', len(orders_product_info))
+# orders_common_titles_info_unsplit = map_product_code_2_common_titles(orders_product_info)
+#
+# suits_product_info = get_suits_product_info()
+# orders_product_info_split = split_suit_product(orders_product_info, suits_product_info)
+# orders_common_titles_info_split = map_product_code_2_common_titles(orders_product_info_split)
+#
+# with open('orders_common_titles_info_unsplit.json', 'w', encoding='utf8') as fp:
+#     json.dump(orders_common_titles_info_unsplit, fp)
+#
+# with open('orders_common_titles_info_split.json', 'w', encoding='utf8') as fp:
+#     json.dump(orders_common_titles_info_split, fp)
+
+# with open('orders_common_titles_info_unsplit.json', 'r', encoding='utf8') as file:
+#     ddd = json.load(file)
+#     print(ddd)
+
+with open('orders_common_titles_info_split.json', 'r', encoding='utf8') as file:
+    ddd = json.load(file)
+    # print(ddd)
+
+transactions = []
+cnt = 0
+for code in ddd:
+    transactions.append(tuple(ddd[code]))
+    cnt += 1
+    if cnt > 300:
+        break
+
+print(transactions)
+
+# transactions = [('eggs', 'bacon', 'soup'),
+#                 ('eggs', 'bacon', 'apple'),
+#                 ('soup', 'bacon', 'banana')]
+
+itemsets, rules = apriori(transactions, min_support=0.005,  min_confidence=0.05)
+print(rules)  # [{eggs} -> {bacon}, {soup} -> {bacon}]
 
 
-
-# ddd = get_suits_product_info()
-
-# print('ddd[796319] is ', ddd[796319])
-# print('ddd[799223] is ', ddd[799223])
-
-eee = get_orders_product_info()
-print('eee is ', len(eee))
