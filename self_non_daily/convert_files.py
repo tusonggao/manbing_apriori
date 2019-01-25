e43618152fd7192765b33db570894d2b74eb3899
tusonggao <tusonggao@163.com>
2019-01-25 14:55:09 +0800

    restructre repo

---
+++ self_non_daily/convert_files.py
@@ -0,0 +1,79 @@
+# import efficient_apriori
+# from efficient_apriori import apriori
+
+import pandas as pd
+import numpy as np
+import time
+import json
+
+from apriori_in_actions import *
+from collections import OrderedDict
+
+
+useless_common_titles = {'售后服务卡', '健客大药房旗舰店宣传单', '健客家庭药箱',
+                         '名流水晶狼牙套', '杜蕾斯', '人体润滑液', '枸橼酸西地那非片',
+                         '避孕套', '男用湿巾', '人初抑菌喷雾剂'}
+
+
+def strip_useless_titles(titles_list):
+    titles_list_new = []
+
+    for title in titles_list:
+        flag = True
+        for useless_title in useless_common_titles:
+            if title.find(useless_title)!=-1:
+                flag = False
+                break
+        if flag:
+            titles_list_new.append(title)
+        else:
+            print('title: ', title)
+
+    return titles_list_new
+
+def convert_transactions_files():
+    transactions = []
+    with open('orders_common_titles_info_split.json', 'r', encoding='utf8') as file:
+        orders_common_titles = json.load(file)
+
+    skip_num = 0
+    # with open('./samples_ttt.txt', 'w', encoding='utf8') as file:
+    with open('./samples_full.txt', 'w', encoding='utf8') as file:
+        cnt = 0
+        for code in orders_common_titles:
+            cnt += 1
+            products = [str(ele).strip() for ele in orders_common_titles[code]]
+            products = set(products)
+            products = list(products)
+            products = strip_useless_titles(products)
+            if len(products)==0:
+                print('skip one transaction')
+                skip_num += 1
+                continue
+            content = '###'.join(products)
+            file.write(content+'\n')
+            if cnt>=10000:
+                continue
+
+    print('skip_num is ', skip_num)
+    print('convert_transactions_files file write ok')
+
+
+def check_transactions_files():
+    with open('./samples_full.txt', 'r', encoding='utf8') as file:
+        cnt = 0
+        for line in file:
+            cnt += 1
+            sss = line.strip().split('###')
+            if len(set(sss))!=len(sss):
+                print('cnt is ', cnt, 'sss is ', sss)
+
+    print('check_transactions_files file write ok')
+
+
+if __name__=='__main__':
+    convert_transactions_files()
+    check_transactions_files()
+
+
+
---
+++ self_non_daily/read_from_impala.py
@@ -0,0 +1,62 @@
+import os
+import json
+import time
+import pandas as pd
+from sqlalchemy import create_engine
+
+###############################################################
+
+def read_sql_script(sql_file):
+    sql_str = ''
+    with open(sql_file, encoding='UTF-8') as file:
+        for line in file:
+            content = line.strip()
+            if content.startswith('--'):  # 过滤注释行
+                continue
+            if content.find('/*')!=-1 and content.find('*/')!=-1:
+                start_idx = content.index('/*')
+                end_idx = content.index('*/')
+                print('start_idx, end_idx ', start_idx, end_idx)
+                content = content[:start_idx] + content[end_idx+2:]
+            sql_str += ' ' + content
+    return sql_str
+
+
+def basefilename(full_file_name):
+    file_name = os.path.split(full_file_name)[-1]
+    raw_file_name = os.path.splitext(file_name)[0]
+    print('raw_file_name is', raw_file_name)
+    return raw_file_name
+
+
+def read_and_store_df_from_impala(sql_file):
+    print('read_and_store_df_from_impala: ', sql_file)
+
+    start_t = time.time()
+    conn_impala = create_engine('impala://172.21.57.127:21050')
+    sql = read_sql_script(sql_file)
+    print('sql is ', sql)
+    df = pd.read_sql(sql, conn_impala)
+    end_t = time.time()
+
+    print('read data from impala cost time ', end_t-start_t)
+    print('df.shape is', df.shape)
+    print('df.head() is', df.head())
+
+    output_file = basefilename(sql_file) + '_data.csv'
+    df.to_csv('./data/'+output_file, index=0)
+
+    return df
+
+
+
+df = read_and_store_df_from_impala('./sql_scripts/hive_sql_orders_products.txt')
+# df = read_and_store_df_from_impala('./sql_scripts/hive_sql_products_info.txt')
+# df = read_and_store_df_from_impala('./sql_scripts/hive_sql_suits_info.txt')
+
+
+print('program ends')
+
+
+
+
